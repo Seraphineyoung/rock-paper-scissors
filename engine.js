4a53a8e8065384488d79a17183539a4cffc812f2
Seraphine Young <seraphine_ene@yahoo.com>
2018-11-26 15:06:37 +0000

    Added images to my page

--- engine.js
+++ engine.js
@@ -5,18 +5,18 @@ var won = 0;
 var lost = 0;
 var draw = 0;
 var played = 0;
-
+var userIcon = $("#you_images");
+var computerIcon = $("#computer_images");
 
 function computer_move() {
+  computerIcon.empty();
   var play_machine = Math.floor(Math.random() * 3);
   computer_play = choices[play_machine];
 $("#computer_move").html(" " + computer_play);
-
-// $(#my_images).append('<img src="css\' + computer_play + png '"></img>')
+$("#computer_images").append("<img src='css/" + computer_play + ".png '/>");
 }
 
 function compare(you, machine) {
-
   var verdict;
   $("body").removeClass();
 
@@ -58,11 +58,10 @@ $("#verdict").html(" " + verdict);
 $(".mybutton").click(function(event) {
   // remove btnColorcolor class from all .mybutton
   $(".mybutton").removeClass("btnColor");
+  userIcon.empty();
   var play_you = $(this).val();
   $("#play_you").html(" " + play_you);
-
-
-
+  $("#you_images").append("<img src='css/" + play_you + ".png '/>");
   computer_move();
   compare(play_you, computer_play)
   $(this).addClass("btnColor");
